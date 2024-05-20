3e92c3b69fd6fefa3cd10af5971ba4b77197ebde
Dru Still <eugene_still@brown.edu>
2024-05-20 00:45:03 -0500

    this needs work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { FaInstagram } from "react-icons/fa6";
  */
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen bg-zinc-800 text-gray-200 ">
+    <main className="flex flex-col min-h-screen bg-base text-white ">
       {/** Navbar & Logo & Search icon */}
       <span className="fixed inset-0 z-20 h-16 ">
         <HeaderNavigation />
@@ -21,25 +21,25 @@ export default function Home() {
       <header className="relative flex flex-col items-center md:min-h-[55vh] min-h-screen w-full">
         <div
           className="absolute inset-0 bg-cover hidden md:flex"
-          style={{ backgroundImage: "url('/DevHeroUpped.webp')" }}
+          style={{ backgroundImage: "url('/HeroUpped.webp')" }}
         ></div>
-        <div className="absolute inset-0 bg-cover bg-zinc-900/80 bg-opacity-80"></div>
+        <div className="absolute blur-xl"></div>
         <MannersIntroductionParent />
       </header>
 
       {/** About Us section with Photo Carousel */}
-      <section className="relative flex flex-col items-center w-full min-h-[60vh] gap-y-24 bg-zinc-950/75">
+      <section className="relative flex flex-col items-center w-full min-h-[60vh] gap-y-24 bg-highlight">
         <AboutUs />
         <PhotoCards />
       </section>
 
       {/** Footer with Social links */}
-      <footer className="relative flex flex-col min-h-[6vh] bg-transparent">
+      <footer className="relative flex flex-col min-h-[7vh] bg-bgsecondary">
         <div className="flex items-center">
-          <div className="w-full text-sm flex pl-32 pt-2 justify-start items-center gap-x-2 font-extralight">
+          <div className="w-full text-sm flex pl-32 pt-6 justify-start items-center gap-x-2 font-extralight">
             Modern Manners of Tulsa &copy; 2016 - 2024
           </div>
-          <div className="w-full flex pr-32 pt-2 justify-end items-center gap-x-2">
+          <div className="w-full flex pr-32 pt-6 justify-end items-center gap-x-2">
             <FaFacebookSquare className="text-xl rounded-lg text-zinc-200" />
             <FaInstagram className="text-xl rounded-lg text-zinc-200" />
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -19,6 +19,12 @@ const config = {
     },
     extend: {
       colors: {
+        base: "#1C1614",
+        bgprimary: "#3D2C2B",
+        bgsecondary: "#534C42",
+        accent: "#3E3D2B",
+        highlight: "#6C6B4B",
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -75,6 +81,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
